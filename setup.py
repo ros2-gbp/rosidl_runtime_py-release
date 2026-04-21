3da11bcdb40f201d9a5606fd5bbc776a939e6994
Scott K Logan <logans@cottsay.net>
2026-04-21 08:30:38 -0500

    Rebase from 'debian/rolling/rosidl_runtime_py'

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ package_name = 'rosidl_runtime_py'
 
 setup(
     name=package_name,
-    version='0.15.1',
+    version='0.15.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
     ],
+    package_data={'': ['py.typed']},
     zip_safe=False,
     author='Dirk Thomas',
     author_email='dthomas@osrfoundation.org',
